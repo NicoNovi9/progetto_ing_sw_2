8aca926bf33132152591ed26e6f8493eee4ab2e1
NicoNovi9 <58565864+NicoNovi9@users.noreply.github.com>
2024-06-29 12:06:36 +0200

    factor clean up

--- src/main/java/model/Factor.java
+++ src/main/java/model/Factor.java
@@ -1,6 +1,4 @@
 package model;
 
-// Per i record:
-// Non è necessario definire esplicitamente i metodi getter, equals, hashCode e toString.
 public record Factor(String first, double second, String third) {
 }
